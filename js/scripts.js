3462c24291ebe839c775d0a7ceb0780c0599e3dc
michaelshamrell <michaelshamrell2001@gmail.com>
2022-01-18 16:35:22 -0800

    WIP
    co-authored-by: Mohmed Nur <moeilly0114@gmail.com>
    Co-Authored-By: Kevin Williams <kevin@k9w.org>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    const foodInput = $("#food").val();
-    const showInput = $("#show").val();
-    const phoneBrandInput = $("#phoneBrand").val();
+    const foodInput = $("#food").val(); // "apple"
+    const showInput = $("#show").val(); // "fringe"
+    const phoneBrandInput = $("#phoneBrand").val(); // "pixel"
   
     $(".food").append(foodInput);
     $(".show").append(showInput);
     $(".phoneBrand").append(phoneBrandInput);
 
     $("#results").show();
+    $("#displayArray").show();
 
-    let array = ["burrito", "taco", "survivor", "chopped"];
-    array.push(phoneBrandArray[0, 1]); // "taco"
+    let array = [foodInput, showInput, phoneBrandInput]; // ["apple", "fringe", "pixel"]
+    array.forEach(); // "taco"
     
-    const phoneBrandArray = ["apple", "android"];
     
-    });
-
   });
 });
+
+/*
+<p id="display-array"></p>
+      <script type="text/javascript">
+        var array = [0, 1, 2, 3, 4, 5];
+        document.getElementById("display-array").textContent = array.join(", ");
+
+/</script>
+*/
